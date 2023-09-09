0cc5965cb0e8cd0a50dc1bb88b40fbfcd6148d7e
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2023-09-09 18:51:11 +0300

    Init

---
+++ src/c++/settings.c++
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------------
+ * Cogwheel - Application settings and config library
+ * Copyright (C) 2023 whs31.
+ *
+ * github.com/whs31/cogwheel
+ * ---------------------------------------------------------------------- */
+
+#include "settings.h"
+
+namespace Cogwheel
+{
+} // Cogwheel
---
+++ src/c++/settings.h
@@ -0,0 +1,16 @@
+/* ---------------------------------------------------------------------
+ * Cogwheel - Application settings and config library
+ * Copyright (C) 2023 whs31.
+ *
+ * github.com/whs31/cogwheel
+ * ---------------------------------------------------------------------- */
+
+#pragma once
+
+namespace Cogwheel
+{
+  class Settings
+  {
+
+  };
+} // Cogwheel
